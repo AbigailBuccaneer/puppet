f006ed50af167e4a49b1d52aaf45288cdc76949d
AbigailBuccaneer <AbigailBuccaneer@users.noreply.github.com>
2016-04-11 16:55:00 +0100

    Format manifests/default.pp with puppet-lint

--- manifests/default.pp
+++ manifests/default.pp
@@ -3,18 +3,18 @@ include '::ntp'
 include '::logrotate'
 
 user { 'abigail':
-    ensure => 'present',
-    managehome => true,
-    groups => ['sudo'],
+  ensure     => present,
+  managehome => true,
+  groups     => ['sudo'],
 }
 
 file_line { 'sudo_group_rule':
-    path => '/etc/sudoers',
-    match => '%sudo',
-    line => '%sudo ALL=(ALL:ALL) ALL',
+  path  => '/etc/sudoers',
+  match => '%sudo',
+  line  => '%sudo ALL=(ALL:ALL) ALL',
 }
 
-package { 'vim': ensure => 'latest' }
+package { 'vim': ensure => latest }
 $vimrc =
 'source /usr/local/share/vim/bundle/vim-pathogen/autoload/pathogen.vim
 execute pathogen#infect("/usr/local/share/vim/bundle/{}")
@@ -22,25 +22,25 @@ syntax on
 filetype plugin indent on'
 file { '/etc/vim/vimrc.local': content => $vimrc }
 
-package { 'git': ensure => 'latest' }
+package { 'git': ensure => latest }
 
-file { '/usr/local/share/vim': ensure => 'directory' }
-file { '/usr/local/share/vim/bundle': ensure => 'directory' }
+file { '/usr/local/share/vim': ensure => directory }
+file { '/usr/local/share/vim/bundle': ensure => directory }
 
 vcsrepo { '/usr/local/share/vim/bundle/vim-pathogen':
-    ensure => 'latest',
-    provider => 'git',
-    source => 'https://github.com/tpope/vim-pathogen.git',
+  ensure   => latest,
+  provider => 'git',
+  source   => 'https://github.com/tpope/vim-pathogen.git',
 }
 
 vcsrepo { '/usr/local/share/vim/bundle/sensible.vim':
-    ensure => 'latest',
-    provider => 'git',
-    source => 'https://github.com/tpope/vim-sensible.git',
+  ensure   => latest,
+  provider => 'git',
+  source   => 'https://github.com/tpope/vim-sensible.git',
 }
 
 vcsrepo { '/usr/local/share/vim/bundle/vim-puppet':
-    ensure => 'latest',
-    provider => 'git',
-    source => 'https://github.com/rodjek/vim-puppet.git',
+  ensure   => latest,
+  provider => 'git',
+  source   => 'https://github.com/rodjek/vim-puppet.git',
 }
